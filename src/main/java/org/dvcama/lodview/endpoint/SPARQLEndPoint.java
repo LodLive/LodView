1bee4008ddcc8f3bea40e3e6e59d9eb633dc954a
Diego Valerio Camarda <diego@lodlive.it>
2016-03-25 20:54:14 +0100

    avoided "too many open files" due to intense use

--- src/main/java/org/dvcama/lodview/endpoint/SPARQLEndPoint.java
+++ src/main/java/org/dvcama/lodview/endpoint/SPARQLEndPoint.java
@@ -35,8 +35,7 @@ public class SPARQLEndPoint {
 		// TODO Auto-generated constructor stub
 	}
 
-	public List<TripleBean> doQuery(String IRI, String aProperty, int start, List<String> queries, String filter,
-			String overrideProperty) throws Exception {
+	public List<TripleBean> doQuery(String IRI, String aProperty, int start, List<String> queries, String filter, String overrideProperty) throws Exception {
 		// System.out.println("executing query on " + conf.getEndPointUrl());
 		List<TripleBean> results = new ArrayList<TripleBean>();
 		HttpAuthenticator auth = null;
@@ -46,21 +45,22 @@ public class SPARQLEndPoint {
 		for (String query : queries) {
 			// System.out.println("-- " + parseQuery(query, IRI, aProperty,
 			// start, filter));
-			QueryExecution qe = QueryExecutionFactory.sparqlService(conf.getEndPointUrl(),
-					parseQuery(query, IRI, aProperty, start, filter), auth);
+			QueryExecution qe = QueryExecutionFactory.sparqlService(conf.getEndPointUrl(), parseQuery(query, IRI, aProperty, start, filter), auth);
 			results = moreThenOneQuery(qe, results, 0, overrideProperty);
+			qe.close();
 		}
 
 		if (results.size() == 0) {
 			if (IRI != null) {
 				boolean hasInverses = false;
 				for (String query : conf.getDefaultInversesTest()) {
-					//System.out.println("query!!! " + parseQuery(query, IRI, aProperty, start, filter));
-					QueryExecution qe = QueryExecutionFactory.sparqlService(conf.getEndPointUrl(),
-							parseQuery(query, IRI, aProperty, start, filter), auth);
+					// System.out.println("query!!! " + parseQuery(query, IRI,
+					// aProperty, start, filter));
+					QueryExecution qe = QueryExecutionFactory.sparqlService(conf.getEndPointUrl(), parseQuery(query, IRI, aProperty, start, filter), auth);
 					if (!hasInverses) {
 						hasInverses = qe.execAsk();
 					}
+					qe.close();
 				}
 				if (!hasInverses) {
 					throw new Exception("404 - not found");
@@ -70,8 +70,7 @@ public class SPARQLEndPoint {
 		return results;
 	}
 
-	private List<TripleBean> moreThenOneQuery(QueryExecution qe, List<TripleBean> results, int retry,
-			String overrideProperty) throws Exception {
+	private List<TripleBean> moreThenOneQuery(QueryExecution qe, List<TripleBean> results, int retry, String overrideProperty) throws Exception {
 
 		try {
 			ResultSet rs = qe.execSelect();
@@ -157,66 +156,71 @@ public class SPARQLEndPoint {
 		return doLocalQuery(model, IRI, null, -1, queries, null);
 	}
 
-	public List<TripleBean> doLocalQuery(Model model, String IRI, String localProperty, int start, List<String> queries,
-			String overrideProperty) throws Exception {
+	public List<TripleBean> doLocalQuery(Model model, String IRI, String localProperty, int start, List<String> queries, String overrideProperty) throws Exception {
 		// System.out.println("executing query on model based on " + IRI);
 		List<TripleBean> results = new ArrayList<TripleBean>();
 
 		for (String query : queries) {
 			QueryExecution qe = QueryExecutionFactory.create(parseQuery(query, IRI, localProperty, start, null), model);
-			ResultSet rs = qe.execSelect();
-			while (rs.hasNext()) {
-				TripleBean rb = new TripleBean();
-				QuerySolution qs = rs.next();
-				String property = "";
-				if (overrideProperty != null) {
-					property = overrideProperty;
-				} else if (qs.get("p") != null) {
-					property = qs.get("p").asNode().toString();
-				}
-				try {
-					if (qs.get("s") != null && !qs.get("s").asNode().toString().startsWith("http://")) { // probably
-						// blanknode
-						rb.setIRI(qs.get("s").asNode().toString());
-						rb.setNsIRI("_:" + rb.getIRI());
-					} else if (qs.get("s") != null && qs.get("s").asNode().toString().startsWith("http://")) {
-						rb.setIRI(qs.get("s").asNode().toString());
-						rb.setNsIRI(Misc.toNsResource(rb.getIRI(), conf));
-						rb.setUrl(Misc.toBrowsableUrl(rb.getIRI(), conf));
+			try {
+				ResultSet rs = qe.execSelect();
+				while (rs.hasNext()) {
+					TripleBean rb = new TripleBean();
+					QuerySolution qs = rs.next();
+					String property = "";
+					if (overrideProperty != null) {
+						property = overrideProperty;
+					} else if (qs.get("p") != null) {
+						property = qs.get("p").asNode().toString();
 					}
-					PropertyBean p = new PropertyBean();
-					p.setNsProperty(Misc.toNsResource(property, conf));
-					p.setProperty(property);
-					p.setPropertyUrl(Misc.toBrowsableUrl(property, conf));
-					if (ontoBean != null) {
-						p.setLabel(ontoBean.getEscapedValue("label", locale, property));
-						p.setComment(ontoBean.getEscapedValue("comment", locale, property));
-					}
-					rb.setProperty(p);
-					if (qs.get("o") != null) {
-						Node object = qs.get("o").asNode();
-						if (object.isURI()) {
-							rb.setType("iri");
-							rb.setValue(object.toString(false));
-						} else if (object.isLiteral()) {
-							rb.setType("literal");
-							rb.setDataType(object.getLiteralDatatypeURI());
-							rb.setNsDataType(Misc.toNsResource(object.getLiteralDatatypeURI(), conf));
-							rb.setLang(object.getLiteralLanguage());
-							rb.setValue(object.getLiteralLexicalForm());
-						} else if (object.isBlank()) {
-							rb.setType("bnode");
-							rb.setValue(object.toString(false));
+					try {
+						if (qs.get("s") != null && !qs.get("s").asNode().toString().startsWith("http://")) { // probably
+							// blanknode
+							rb.setIRI(qs.get("s").asNode().toString());
+							rb.setNsIRI("_:" + rb.getIRI());
+						} else if (qs.get("s") != null && qs.get("s").asNode().toString().startsWith("http://")) {
+							rb.setIRI(qs.get("s").asNode().toString());
+							rb.setNsIRI(Misc.toNsResource(rb.getIRI(), conf));
+							rb.setUrl(Misc.toBrowsableUrl(rb.getIRI(), conf));
 						}
-					} else {
-						rb.setType("literal");
-						rb.setValue("");
+						PropertyBean p = new PropertyBean();
+						p.setNsProperty(Misc.toNsResource(property, conf));
+						p.setProperty(property);
+						p.setPropertyUrl(Misc.toBrowsableUrl(property, conf));
+						if (ontoBean != null) {
+							p.setLabel(ontoBean.getEscapedValue("label", locale, property));
+							p.setComment(ontoBean.getEscapedValue("comment", locale, property));
+						}
+						rb.setProperty(p);
+						if (qs.get("o") != null) {
+							Node object = qs.get("o").asNode();
+							if (object.isURI()) {
+								rb.setType("iri");
+								rb.setValue(object.toString(false));
+							} else if (object.isLiteral()) {
+								rb.setType("literal");
+								rb.setDataType(object.getLiteralDatatypeURI());
+								rb.setNsDataType(Misc.toNsResource(object.getLiteralDatatypeURI(), conf));
+								rb.setLang(object.getLiteralLanguage());
+								rb.setValue(object.getLiteralLexicalForm());
+							} else if (object.isBlank()) {
+								rb.setType("bnode");
+								rb.setValue(object.toString(false));
+							}
+						} else {
+							rb.setType("literal");
+							rb.setValue("");
+						}
+						results.add(rb);
+					} catch (Exception e) {
+						System.err.println("error? " + e.getMessage());
+						e.printStackTrace();
 					}
-					results.add(rb);
-				} catch (Exception e) {
-					System.err.println("error? " + e.getMessage());
-					e.printStackTrace();
 				}
+			} catch (Exception e) {
+				throw new Exception("500 - " + e.getMessage());
+			} finally {
+				qe.close();
 			}
 		}
 
@@ -227,11 +231,12 @@ public class SPARQLEndPoint {
 	}
 
 	public Model extractData(Model result, String IRI, String sparql, List<String> queries) throws Exception {
-		try {
-			// System.out.println("executing query on " + sparql);
-			Resource subject = result.createResource(IRI);
-			for (String query : queries) {
-				QueryExecution qe = QueryExecutionFactory.sparqlService(sparql, parseQuery(query, IRI, null, -1, null));
+
+		// System.out.println("executing query on " + sparql);
+		Resource subject = result.createResource(IRI);
+		for (String query : queries) {
+			QueryExecution qe = QueryExecutionFactory.sparqlService(sparql, parseQuery(query, IRI, null, -1, null));
+			try {
 				ResultSet rs = qe.execSelect();
 
 				List<Statement> sl = new ArrayList<Statement>();
@@ -240,40 +245,42 @@ public class SPARQLEndPoint {
 					RDFNode subject2 = qs.get("s");
 					RDFNode property = qs.get("p");
 					RDFNode object = qs.get("o");
-					result.add(result.createStatement(subject2 != null ? subject2.asResource() : subject,
-							property.as(Property.class), object));
+					result.add(result.createStatement(subject2 != null ? subject2.asResource() : subject, property.as(Property.class), object));
 				}
 				result.add(sl);
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new Exception("error in query execution: " + e.getMessage());
+			} finally {
+				qe.close();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new Exception("error in query execution: " + e.getMessage());
 		}
 		return result;
 	}
 
 	public Model extractLocalData(Model result, String IRI, Model m, List<String> queries) throws Exception {
-		try {
-			// System.out.println("executing query on IRI");
-			Resource subject = result.createResource(IRI);
-			for (String query : queries) {
-				QueryExecution qe = QueryExecutionFactory.create(parseQuery(query, IRI, null, -1, null), m);
-				ResultSet rs = qe.execSelect();
 
+		// System.out.println("executing query on IRI");
+		Resource subject = result.createResource(IRI);
+		for (String query : queries) {
+			QueryExecution qe = QueryExecutionFactory.create(parseQuery(query, IRI, null, -1, null), m);
+			try {
+				ResultSet rs = qe.execSelect();
 				List<Statement> sl = new ArrayList<Statement>();
 				while (rs.hasNext()) {
 					QuerySolution qs = rs.next();
 					RDFNode subject2 = qs.get("s");
 					RDFNode property = qs.get("p");
 					RDFNode object = qs.get("o");
-					result.add(result.createStatement(subject2 != null ? subject2.asResource() : subject,
-							property.as(Property.class), object));
+					result.add(result.createStatement(subject2 != null ? subject2.asResource() : subject, property.as(Property.class), object));
 				}
 				result.add(sl);
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new Exception("error in query execution: " + e.getMessage());
+			} finally {
+				qe.close();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new Exception("error in query execution: " + e.getMessage());
 		}
 		return result;
 	}
@@ -300,20 +307,23 @@ public class SPARQLEndPoint {
 	public String testEndpoint(ConfigurationBean conf) {
 		System.out.println("testing connection on " + conf.getEndPointUrl());
 		QueryExecution qe = QueryExecutionFactory.sparqlService(conf.getEndPointUrl(), "select ?s {?s ?p ?o} LIMIT 1");
-
+		String msg = "";
 		try {
 			ResultSet rs = qe.execSelect();
 			if (rs.hasNext()) {
 				System.out.println("is online");
-				return "online";
+				msg = "online";
 			} else {
 				System.out.println("is offline");
-				return "offline";
+				msg = "offline";
 			}
 		} catch (Exception e) {
 			System.out.println("is offline " + e.getMessage());
-			return "offline " + e.getMessage();
+			msg = "offline " + e.getMessage();
+		} finally {
+			qe.close();
 		}
+		return msg;
 	}
 
 }
